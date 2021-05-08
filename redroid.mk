4ba4ed038b80b858939cae1f6f504d583373ea99
Ziyang Zhou <ziyang.zhou@outlook.com>
2021-05-08 13:34:05 +0000

    add Android 8.1 support

---
+++ redroid.mk
@@ -0,0 +1,90 @@
+# Copyright (C) 2013 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+AUDIOSERVER_MULTILIB := first
+
+# ?
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.radio.noril=yes
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.nobootanimation=1
+
+# required by sync.sh script
+PRODUCT_PACKAGES += \
+    fs_config
+
+PRODUCT_PACKAGES += \
+    vncserver \
+    hwcomposer.redroid \
+	gralloc.gbm \
+    gralloc.redroid \
+    gatekeeper.ranchu \
+    libEGL_swiftshader \
+    libGLESv1_CM_swiftshader \
+    libGLESv2_swiftshader \
+	libGLES_mesa \
+	ipconfigstore \
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.kernel.qemu=1 \
+    ro.hardware=redroid \
+    ro.hardware.gatekeeper=ranchu \
+    ro.sf.lcd_density=320 \
+
+# Phone App required
+PRODUCT_PACKAGES += \
+    rild
+
+# WiFi required by SystemUI
+PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-service
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+
+
+# required HIDL
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-service \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.configstore@1.1-service \
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.0-impl \
+    android.hardware.gatekeeper@1.0-service \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.keymaster@3.0-service \
+    android.hardware.keymaster@3.0-impl \
+
+
+PRODUCT_COPY_FILES += \
+    device/generic/goldfish/camera/media_profiles.xml:system/etc/media_profiles.xml \
+    device/generic/goldfish/camera/media_codecs.xml:system/etc/media_codecs.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+    frameworks/av/media/libeffects/data/audio_effects.conf:system/etc/audio_effects.conf \
+	frameworks/native/data/etc/android.hardware.ethernet.xml:/system/etc/permissions/android.hardware.ethernet.xml \
+    device/generic/goldfish/audio_policy.conf:system/etc/audio_policy.conf
+
+    
+# Extend heap size we use for dalvik/art runtime
+$(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
+
---
+++ redroid_x86_64/BoardConfig.mk
@@ -0,0 +1,16 @@
+include device/generic/x86_64/BoardConfig.mk
+
+TARGET_USES_HWC2 := true
+
+# want all fonts
+SMALLER_FONT_FOOTPRINT := false
+MINIMAL_FONT_FOOTPRINT := false
+
+# use seperate vendor partition
+TARGET_COPY_OUT_VENDOR := vendor
+
+BOARD_GPU_DRIVERS := i915 i965 nouveau kmsro r300g r600g virgl vmwgfx etnaviv iris tegra radeonsi
+
+DEVICE_MANIFEST_FILE := device/redroid/manifest.xml
+PRODUCT_ENFORCE_VINTF_MANIFEST := true
+
