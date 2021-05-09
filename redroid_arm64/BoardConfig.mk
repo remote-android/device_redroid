454d447380a978ce2720581ef0ba750dadf6ca9b
Ziyang Zhou <ziyang.zhou@outlook.com>
2021-05-09 17:38:20 +0000

    Android S preview support

---
+++ redroid_arm64/BoardConfig.mk
@@ -0,0 +1,18 @@
+include device/generic/arm64/BoardConfig.mk
+
+# want all fonts
+SMALLER_FONT_FOOTPRINT := false
+MINIMAL_FONT_FOOTPRINT := false
+
+BUILD_EMULATOR_OPENGL := false
+
+# use seperate vendor partition
+TARGET_COPY_OUT_VENDOR := vendor
+
+BOARD_GPU_DRIVERS := freedreno lima
+
+DEVICE_MANIFEST_FILE := device/redroid/manifest.xml
+PRODUCT_ENFORCE_VINTF_MANIFEST := true
+
+# ~ 1.3G
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1388314624
