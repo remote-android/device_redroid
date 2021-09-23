dc4d4c03a662b298dc59d5d1fc62333e5dcbbb64
Ziyang Zhou <ziyang.zhou@outlook.com>
2021-09-24 00:15:41 +0800

    refine product / board definition

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,5 @@
 # This file is executed by build/envsetup.sh, and can use anything
 # defined in envsetup.sh.
 
-add_lunch_combo redroid_x86_64-eng
 add_lunch_combo redroid_x86_64-userdebug
-add_lunch_combo redroid_x86_64-user
-add_lunch_combo redroid_arm64-eng
 add_lunch_combo redroid_arm64-userdebug
-add_lunch_combo redroid_arm64-user
