c4686a17b2ada55733ce2a1074180bef22bf81cd
Ziyang Zhou <ziyang.zhou@outlook.com>
2021-11-08 21:30:20 +0800

    update drm hal

--- redroid.mk
+++ redroid.mk
@@ -91,9 +91,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio@4.0-impl \
     android.hardware.audio.effect@4.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.3-service.clearkey \
+    android.hardware.drm@1.4-service-lazy.clearkey \
     android.hardware.gatekeeper@1.0-service.software \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.allocator@2.0-impl \
