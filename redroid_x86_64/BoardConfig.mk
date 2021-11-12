72296451666a22778f8426eda299b3a3e5fe2a7b
Ziyang Zhou <ziyang.zhou@outlook.com>
2021-11-12 21:35:23 +0800

    fix DEVICE_MANIFEST_FILE var

--- redroid_x86_64/BoardConfig.mk
+++ redroid_x86_64/BoardConfig.mk
@@ -18,5 +18,5 @@ BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 
 BOARD_GPU_DRIVERS := i915 i965 r300g r600g virgl iris radeonsi
 
-DEVICE_MANIFEST_FILE := device/redroid/manifest.xml
+DEVICE_MANIFEST_FILE += device/redroid/manifest.xml
 PRODUCT_ENFORCE_VINTF_MANIFEST := true
