3083e8b43d05e6ac700844722ef37d71b4be4040
Ziyang Zhou <ziyang.zhou@outlook.com>
2021-12-20 21:10:55 +0800

    add broadcom / freedreno / panfrost SoC support (NOT TESTED :( )

--- redroid_arm64.mk
+++ redroid_arm64.mk
@@ -16,6 +16,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 $(call inherit-product, $(LOCAL_PATH)/redroid.mk)
+$(call inherit-product, $(LOCAL_PATH)/redroid_arm64/device.mk)
 
 # overrides
 PRODUCT_NAME := redroid_arm64
