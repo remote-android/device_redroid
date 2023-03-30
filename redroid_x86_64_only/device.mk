3b59c7498a385503d9334a55aa1637230d42ef57
Ziyang Zhou <ziyang.zhou@outlook.com>
2023-03-30 20:38:32 +0800

    add native bridge support for x86_64

--- redroid_x86_64_only/device.mk
+++ redroid_x86_64_only/device.mk
@@ -1,4 +1,9 @@
 PRODUCT_COPY_FILES += \
     device/redroid/mediacodec.policy.x86:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.enable.native.bridge.exec=1 \
+    ro.dalvik.vm.isa.arm64=x86_64 \
+    ro.dalvik.vm.native.bridge=libnb.so \
+
 $(call inherit-product, device/redroid-prebuilts/prebuilts_x86.mk)
