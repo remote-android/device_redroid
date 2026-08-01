d686dd0bbc001f866c4a5750cd6cd9b3d5a05c4b
Rubén Díaz <ruben.diaz@synkross.com>
2026-08-01 11:14:36 +0200

    support Android 17
    Bump PRODUCT_MODEL to redroid17_* and switch the lunch targets to the
    cp2a release configuration (SDK 37).

--- redroid_arm64_only.mk
+++ redroid_arm64_only.mk
@@ -24,7 +24,7 @@ $(call inherit-product, $(LOCAL_PATH)/redroid_arm64_only/device.mk)
 PRODUCT_NAME := redroid_arm64_only
 PRODUCT_DEVICE := redroid_arm64_only
 PRODUCT_BRAND := redroid
-PRODUCT_MODEL := redroid16_arm64_only
+PRODUCT_MODEL := redroid17_arm64_only
 
 DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlay
 
